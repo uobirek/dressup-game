c5c8419533cb370a2024a7d6262f28256ed2168f
Ula <urszula.obirek@gmail.com>
2025-02-27 15:57:31 +0100

    added more assets

--- script.js
+++ script.js
@@ -53,3 +53,50 @@ function initCharacter() {
 }
 
 window.onload = initCharacter;
+const dialogLines = [
+    "Welcome to the dress-up game!",
+    "Click on the buttons to choose your outfit.",
+    "Try out different styles and have fun!",
+    "Looking great! What will you wear next?",
+    "You're a fashion star!",
+];
+
+let currentLine = 0;
+let typing = false;
+let interval;
+
+// Function to type text letter by letter
+function printLetterByLetter(destination, message, speed, callback) {
+    let i = 0;
+    document.getElementById(destination).innerHTML = "";
+
+    typing = true;
+    interval = setInterval(function () {
+        document.getElementById(destination).innerHTML += message.charAt(i);
+        i++;
+        if (i >= message.length) {
+            clearInterval(interval);
+            typing = false;
+            if (callback) callback();
+        }
+    }, speed);
+}
+
+// Function to show next line of dialog
+function nextDialog() {
+    if (typing) return;
+
+    if (currentLine < dialogLines.length) {
+        printLetterByLetter("msg", dialogLines[currentLine], 50);
+        currentLine++;
+    } else {
+        document.getElementById("dialog-box").style.display = "none";
+    }
+}
+
+// Start the dialog when the document is ready
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("dialog-box").addEventListener("click", nextDialog);
+    nextDialog();
+});
+
